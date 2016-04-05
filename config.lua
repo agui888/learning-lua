683e5fe7ba202317c1ba30aae2c667fca19faec9
zhourunlai <u201316277@hust.edu.cn>
2016-04-05 22:59:44 +0800

    lua

---
+++ config.lua
@@ -0,0 +1,26 @@
+--
+-- User: aaron
+-- Date: 4/5/16
+-- Time: 20:58
+--
+
+local M = {}
+
+local redis = {}
+redis['host'] = '127.0.0.1'
+redis['port'] = 6379
+redis['password'] = 'wepieteam87542709'
+
+local err_msg = {}
+err_msg[42] = 'URL is invalid'
+err_msg[51] = 'Redis is out of service'
+err_msg[52] = 'Getting data error'
+
+local default = {}
+default['url'] = 'http://fanqie.im'
+
+M['redis'] = redis
+M['err_msg'] = err_msg
+M['default'] = default
+
+return M
---
+++ index.lua
@@ -0,0 +1,18 @@
+--
+-- User: aaron
+-- Date: 4/5/16
+-- Time: 18:04
+--
+ngx.header.content_type = 'text/html'
+
+local config = require 'config'
+local functions = require 'functions'
+
+local short_string = ngx.re.sub(ngx.var.uri, "^/u/(.*)", "$1", "o")
+
+local long_url, err = functions.get_long_url(short_string)
+if err then
+    functions.show_err(err)
+end
+
+ngx.redirect(long_url)

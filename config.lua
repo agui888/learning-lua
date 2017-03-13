c74c6859c4155ee2b28b484f34abfd19c7a10378
zhourunlai <xiaorun95@gmail.com>
2017-03-13 14:00:24 +0800

    update

--- config.lua
+++ config.lua
@@ -9,7 +9,7 @@ local M = {}
 local redis = {}
 redis['host'] = '127.0.0.1'
 redis['port'] = 6379
-redis['password'] = ‘*****’
+redis['password'] = '****'
 redis['prefix'] = 'url_service:'
 
 local err_msg = {}
